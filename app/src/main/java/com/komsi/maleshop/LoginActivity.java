86706bf6f30dba71b3270437a5a6155b75d0fb7c
Rohmanseo <rohmann311@gmail.com>
2019-11-05 09:38:32 +0700

    Menambahkan intent di login activity

--- app/src/main/java/com/komsi/maleshop/LoginActivity.java
+++ app/src/main/java/com/komsi/maleshop/LoginActivity.java
@@ -2,13 +2,26 @@ package com.komsi.maleshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvDaftar = findViewById(R.id.tv_daftar);
+
+        tvDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
     }
 }
