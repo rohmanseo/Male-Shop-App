6c03e75143a1d546e60ec4167397ca8355addffb
Rizal <rizalmifta79@gmail.com>
2019-11-05 09:46:17 +0700

    menyelesaikan splash

--- app/src/main/java/com/komsi/maleshop/Splash.java
+++ app/src/main/java/com/komsi/maleshop/Splash.java
@@ -2,13 +2,40 @@ package com.komsi.maleshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+    private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        final Intent intent = new Intent(this,LoginActivity.class);
+        Thread timer =new Thread(){
+            public void run (){
+             try{
+                 sleep(2000);
+
+             }catch (InterruptedException e){
+                 e.printStackTrace();
+             }
+             finally {
+                 startActivity(intent);
+                 finish();
+             }
+            }
+
+        };
+        timer.start();
     }
 }
