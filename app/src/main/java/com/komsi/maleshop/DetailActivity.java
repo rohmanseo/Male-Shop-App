c66341b20ae3cc6d4c88897215a5cb578c93722f
Rohmanseo <rohmann311@gmail.com>
2019-11-17 10:31:15 +0700

    Update halaman detail

--- app/src/main/java/com/komsi/maleshop/DetailActivity.java
+++ app/src/main/java/com/komsi/maleshop/DetailActivity.java
@@ -3,12 +3,21 @@ package com.komsi.maleshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
+
+    ImageView imgProduk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        imgProduk = findViewById(R.id.img_produk);
+
+        Glide.with(this).load(R.drawable.produk).into(imgProduk);
     }
 }
