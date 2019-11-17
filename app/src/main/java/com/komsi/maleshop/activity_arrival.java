7ead882d18dcf460f0bf051df7dd504a2865b33b
Rizal <rizalmifta79@gmail.com>
2019-11-17 11:45:03 +0700

    menambahkan produk di kategori

---
+++ app/src/main/java/com/komsi/maleshop/activity_arrival.java
@@ -0,0 +1,14 @@
+package com.komsi.maleshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_arrival extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrival);
+    }
+}
